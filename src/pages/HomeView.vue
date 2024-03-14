d850c2cd4e8bf1806fe7f44c0ec5b1ce6316e409
Aveleira <jose.aveleira@gmail.com>
2024-03-14 11:14:41 +0100

    Interfaz dashboard_IoT Primera Clase Presencial

---
+++ src/pages/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container class="mt-10">
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-slider
+          v-model="value"
+          color="blue"
+          thumb-label
+          label="Slider"
+        ></v-slider>
+      </v-col>
+
+      <v-col cols="12" sm="6">
+        <v-text-field
+          v-model="value"
+          label="Caja de texto"
+          type="number"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const value = ref(50)
+
+    return {
+      value
+    }
+  }
+}
+</script>
+
